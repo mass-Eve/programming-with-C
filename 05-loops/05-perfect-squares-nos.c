81f0bfa5a52e1a129de75c0316e830021f48d862
mass-eve <detoxedankur@gmail.com>
2023-11-20 16:34:13 +0530

    program-5 of hw done

--- 05-loops/05-perfect-squares-nos.c
+++ 05-loops/05-perfect-squares-nos.c
@@ -3,14 +3,14 @@
 
 void main ()
 {
-    int n;
+    int n, i;
 
-    printf("Enter the total no. of terms here:");
+    printf("Enter the no. of terms here:");
     scanf("%d", &n);
 
     for (i=1; i <= n; i++)
     {
-        printf("%d", i*i)
+        printf("%d, ", i*i);
     }
 
     // getch();
