cc9fa49216a1c272d77c575a9db71c2f33041019
mass-eve <detoxedankur@gmail.com>
2023-12-04 21:33:47 +0530

    some more programs completed. Now only 10 programs are left, hopefully!

---
+++ 05-loops/for-loop/28,29/160.c
@@ -0,0 +1,50 @@
+/*
+    1 + 1+x + 1+x+x^2 + 1+x+x^2+x^3
+        ___   ______    ___________  + ......... upto n 
+         2      4            6
+*/
+
+#include <stdio.h>
+// #include<conio.h>
+#include <math.h>
+void main()
+{
+    int i, n, x, power;
+    
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    printf("Enter x: ");
+    scanf("%d", &x);
+
+    float nr = 1, dr, sum = 1;
+
+    for (i = 1; i <= n; i++)
+    {
+        nr += pow(x, i);
+        dr = i * 2 ;
+        sum += nr / dr ;
+    }
+    printf("Sum = %f", sum);
+}
+
+/*
+
+        let x = 2 and n = 3
+
+    i = 1;
+            nr = 1 + pow(2, 1) ==> 3
+            dr = 2
+            sum = 1 + 3/2 ==> 2.5
+
+    i = 2;
+            nr = 1 + pow(2, 1) + pow(2, 2) ==> 7
+            dr = 4
+            sum = 2.5 + 1.75 ==> 4.25
+
+    i = 3;
+            nr = 1 + pow(2, 1) + pow(2, 2) + pow(2, 3) ==> 15
+            dr = 6
+            sum = 4.25 + 2.5 ==> 6.75
+
+*/
---
+++ 05-loops/for-loop/28,29/164.c
@@ -0,0 +1,42 @@
+/*
+    1 + 1+x^2 + 1+x^2+x^9 + 1+x^2+x^9+x^28
+        _____   _______   ___________ + ....... upto n
+          1        4          7
+*/
+
+#include <stdio.h>
+// #include<conio.h>
+#include <math.h>
+void main()
+{
+    int i, n, x, dr = -2;
+
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    printf("Enter x: ");
+    scanf("%d", &x);
+
+    float nr = 1, sum = 1;
+
+    for (i = 1; i <= n; i++)
+    {
+        nr += pow(x, i * i * i + 1);
+        dr += 3;
+
+        sum += nr / dr;
+    }
+    printf("Sum = %f", sum);
+}
+
+/*
+
+        let x == 2 and n == 3
+
+    i = 1;
+            sum = 5 + 1 == 6
+
+    i = 2;
+            sum = 6 + 129.25 ==> 135.25
+
+*/
