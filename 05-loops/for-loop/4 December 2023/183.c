244f6b35a49a79e26c0a04e27e527cc5a755f7b8
mass-eve <detoxedankur@gmail.com>
2023-12-05 18:32:41 +0530

    Wrong question was written, so changed that

--- 05-loops/for-loop/4 December 2023/183.c
+++ 05-loops/for-loop/4 December 2023/183.c
@@ -1,11 +1,10 @@
 // WAP to print the following pattern of numbers :-
 /*
-        1
-        1 2 
-        1 2 3
-        1 2 3 4
-        1 2 3 4 5
-
+        5
+        5 6
+        5 6 7
+        5 6 7 8
+        5 6 7 8 9
 */
 
 #include<stdio.h>
