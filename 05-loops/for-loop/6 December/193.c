cd2c088124a15f624201916a389e2c1e567dd100
mass-eve <detoxedankur@gmail.com>
2023-12-06 16:29:31 +0530

    06 December HW Done

---
+++ 05-loops/for-loop/6 December/193.c
@@ -0,0 +1,30 @@
+/*
+        * * * * * * * * *
+          * * * * * * *
+            * * * * *
+              * * *
+                *
+*/
+
+
+#include <stdio.h>
+// #include<conio.h>
+
+void main()
+{
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int spaces = 1; spaces <= ((i - 1) * 2); spaces++)
+        {
+            printf(" ");
+        }
+
+        for (int stars = 1; stars <= 9 - 2*(i-1); stars++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    // getch();
+}
