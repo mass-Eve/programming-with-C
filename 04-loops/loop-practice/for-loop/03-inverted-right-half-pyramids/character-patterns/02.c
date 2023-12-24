968b3e2acc0f698797667a82c3097a536168f540
mass-eve <detoxedankur@gmail.com>
2023-12-24 15:38:38 +0530

    Day03: right-inverted-half character pyrmmids

---
+++ 04-loops/loop-practice/for-loop/03-inverted-right-half-pyramids/character-patterns/02.c
@@ -0,0 +1,25 @@
+/*
+        * # * # *
+        * # * #
+        * # *
+        * #
+        *
+*/
+
+#include<stdio.h>
+
+void main()
+{
+
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int pos = 1; pos <= 5 - (i -1); pos++)
+        {
+            if (pos % 2 != 0)
+                printf("* ");
+            else
+                printf("# ");
+        }
+        printf("\n");
+    }
+}
