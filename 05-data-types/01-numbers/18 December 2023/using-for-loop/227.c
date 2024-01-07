278dbee4d75f8bf026fc622d95d7201cb09ef7e9
Ankur Agrawal <detoxedankur@gmail.com>
2024-01-07 23:12:44 +0530

    Create 227.c
    sessional practice

---
+++ 05-data-types/01-numbers/18 December 2023/using-for-loop/227.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void main()
+{
+    // printf("Hello World");
+
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    int nc = num;
+    int sum = 0;
+    
+    for (int i = 1; i <= nc; nc/=10)
+    {
+        int rem = nc % 10;
+        sum += rem;
+    }
+    printf("The sum of the individual digits of %d = %d", num, sum);
+}
